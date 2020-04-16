8913b6cb3b4c0a3ef6254f5622144492c172cb78
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-04-17 01:32:17 +0200

    bump version to 0.2.0

--- about.py
+++ about.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """Version information, etc."""
 
-#  DskLayout version info
-__version__ = '0.1.2'
+#  version info
+__version__ = '0.2.0'
 """Package version as a string."""
 
 # Local Variables:
